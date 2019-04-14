59a7b45a473a86a378cf21f94cab8530fdb2c92d
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-14 20:42:27 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-//this file is optional
+//this file is optionalitle>
+var element=document.createElement('div');
+element.innerHTML="Hello,DOM!";
+
+document.body.appendChild(element);
+element.style.textAlign='center';
+var ul=document.createElement('ul');
+for (let i=0; i<3; i++){
+let li=document.createElement('li');
+li.innerHTML=(i+1).toString();
+}
+element.appendChild(ul);
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+ul.remove();</title>
 //you can use this to paste in js code and have it run when index.html is loaded

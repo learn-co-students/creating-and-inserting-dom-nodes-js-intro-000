fdd324f7943121da7c805d8b6c1e9c191554a085
Sasa Trkulja <kamaradsasa@gmail.com>
2019-11-28 12:55:11 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var main = document.getElementById('main');
+var ul = document.createElement('ul');
+for (var i = 0; i < 5; i++) {
+  var li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+main.appendChild(ul);
+ul.removeChild(document.querySelector('#main ul li:nth-child(2)'));

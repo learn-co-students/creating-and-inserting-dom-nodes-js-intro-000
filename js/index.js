731b5919fe4fd6c52cfa5d30bef03d9b4f96b7ec
voluble-block-7446 <iagogomes80@gmail.com>
2020-01-17 18:32:31 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var ele = document.createElement('div');
+ele.innerHTML = 'Hello, DOM!';
+ele.style.backgroundColor = '#f9f9f9';
+document.body.appendChild(ele);
+ele.style.textAlign = 'center';

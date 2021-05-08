d24a9b32377df810a716d3ca3f2934cb73d98358
Robert Killion <robertfkillion@gmail.com>
2021-05-08 12:48:22 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var para = document.createElement('div');
+para.innerHTML = "This might be a paragraph in my work. Maybe I would write a nice letter to the DOM.";
+"This might be a paragraph in my work. Maybe I would write a nice letter to the DOM.";
+document.body.appendChild(para);
